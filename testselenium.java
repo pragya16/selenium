cf1de2c7b1285e931477bac13334849336a51a87
pragya16 <pragya.g16@gmail.com>
2017-11-30 03:15:51 -0800

    test

---
+++ testselenium.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class testSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.setProperty("webdriver.gecko.driver","C:\\selenuim\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		
+		FirefoxOptions options = new FirefoxOptions();
+		options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe"); //This is the location where you have installed Firefox on your machine
+
+ 
+		WebDriver driver = new FirefoxDriver(options);
+		driver.get("https://www.google.com");
+		driver.close();
+		WebDriver driver1 = new FirefoxDriver(options);
+		driver1.get("https://touch.facebook.com/");
+		driver1.close();
+	}
+
+}
